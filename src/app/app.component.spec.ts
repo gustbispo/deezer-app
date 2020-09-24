87018acffe745d6b48b64aa5dfb7f7c8aa8ae053
gbispo <bispos2@hotmail.com>
2020-09-24 19:43:31 -0300

    update name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'deezer-front'`, () => {
+  it(`should have as title 'deezer-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('deezer-front');
+    expect(app.title).toEqual('deezer-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('deezer-front app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('deezer-app app is running!');
   });
 });
