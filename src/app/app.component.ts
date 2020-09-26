222def2e3760d18449bf12a14868fadca081cb41
gbispo <bispos2@hotmail.com>
2020-09-25 21:15:34 -0300

    tablesort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { DeezerService } from './services/deezer.service';
 import { Track } from './models/track';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   pesquisa: string;
   title = 'deezer-front';
   columnsToDisplay = ['img_album', 'nome_album','title','artista', 'rank', 'link'];
-  musicas: Track[];
+  musicas: MatTableDataSource<any>; //Track[];
   showTable: boolean = false;
 
+  @ViewChild(MatSort, null) sort: MatSort;
+
   constructor(private deezerService: DeezerService) {}
+
+  ngAfterViewInit() {
+    
+  }
 
   goToLink(link) {
     window.open(link, '_blank');
@@ -23,7 +31,8 @@ export class AppComponent {
   getTrack(){
     this.showTable = true;
     this.deezerService.getTrack(this.pesquisa).subscribe((musicas: any) => {
-      this.musicas = musicas.data;
+      this.musicas = new MatTableDataSource(musicas.data);
+      this.musicas.sort = this.sort;
     });
   }
 
